f84a6fb48f33befba3ecf478d8e36337ff9cb501
Jignesh Khokhani <khokhanijignesh29@gmail.com>
2023-03-27 20:58:31 +0900

    Implement unsuccessfull exercise functionality

--- spec/features/exercises/creating_exercise_spec.rb
+++ spec/features/exercises/creating_exercise_spec.rb
@@ -2,15 +2,17 @@ require 'rails_helper'
 
 RSpec.feature 'Creating Exercise' do
   let(:user1) { User.create(email: 'user1@example.com', password: 'password') }
-  before { login_as(user1) }
+  before do
+    login_as(user1)
 
-  scenario 'with valid inputs' do
     visit '/'
 
     click_link 'My Lounge'
     click_link 'New Workout'
     expect(page).to have_link('Back')
+  end
 
+  scenario 'with valid inputs' do
     fill_in 'Duration', with: 70
     fill_in 'Workout details', with: 'Weight lifting'
     fill_in 'Activity date', with: '2023-03-27'
@@ -24,13 +26,8 @@ RSpec.feature 'Creating Exercise' do
   end
 
   scenario 'with invalid inputs' do
-    visit '/'
 
-    click_link 'My Lounge'
-    click_link 'New Workout'
-    expect(page).to have_link('Back')
-
-    fill_in 'Duration', with: '50'
+    fill_in 'Duration', with: ''
     fill_in 'Workout details', with: ''
     fill_in 'Activity date', with: ''
     click_button 'Create Exercise'
