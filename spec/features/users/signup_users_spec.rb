8c2ed00e5147d3f647f443b5f26291a1836e2261
Jignesh Khokhani <khokhanijignesh29@gmail.com>
2023-04-01 12:37:37 +0900

    Add first and last name validations to user model

--- spec/features/users/signup_users_spec.rb
+++ spec/features/users/signup_users_spec.rb
@@ -24,4 +24,25 @@ RSpec.feature 'User Signup' do
 
     expect(page).to have_content('u_first u_last')
   end
+
+  scenario 'with invalid credentials' do
+    visit '/'
+
+    click_link 'Sign up'
+
+    fill_in 'First name', with: ''
+    fill_in 'Last name', with: ''
+    fill_in 'Email', with: 'user@example.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+
+    click_button 'Sign up'
+
+    expect(page).to have_content("First name can't be blank")
+    expect(page).to have_content("Last name can't be blank")
+    expect(page).not_to have_link('Sign out')
+
+    expect(page).to have_link('Sign in')
+    expect(page).to have_link('Sign up')
+  end
 end
